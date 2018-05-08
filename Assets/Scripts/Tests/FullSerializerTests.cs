c3705fe8a6d6fed9346604cb862aeef738d3e1c8
konh <konh@yandex.ru>
2018-05-08 21:42:16 +0700

    First case; Complete integration;

---
+++ Assets/Scripts/Tests/FullSerializerTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using FullSerializer;
+
+public class FullSerializerTests {
+
+	[Test]
+	public void OneStringField() {
+		var serializer = new fsSerializer();
+		var obj1 = new TestCases.OneStringField { Field = "some_text" };
+
+		fsData data1;
+		var result1 = serializer.TrySerialize(obj1, out data1);
+		Assert.False(result1.Failed);
+
+		var str = fsJsonPrinter.CompressedJson(data1);
+
+		TestCases.OneStringField obj2 = null;
+		var data2 = fsJsonParser.Parse(str);
+		var result2 = serializer.TryDeserialize<TestCases.OneStringField>(data2, ref obj2);
+		Assert.False(result2.Failed);
+
+		Assert.True(obj1.Field == obj2.Field);
+	}
+}
